86ccfd6600a3034ab941a4f99261f3fcdb7f9e48
vansen345 <86304461+vansen345@users.noreply.github.com>
2023-06-27 09:14:42 +0700

    add input component

---
+++ project_flutter/lib/widgets/input.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:vansen/gen/colors.gen.dart';
+import 'package:vansen/icon/icons.dart';
+
+class InputSearch extends StatelessWidget {
+  const InputSearch(
+      {super.key,
+      required this.textEditingController,
+      required this.onChanged,
+      this.color});
+
+  final Function(String) onChanged;
+  final Color? color;
+  final TextEditingController textEditingController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: VSColors.k686868,
+      textInputAction: TextInputAction.search,
+      controller: textEditingController,
+      style: const TextStyle(
+        fontWeight: FontWeight.w500,
+        color: VSColors.k686868,
+        fontSize: 14,
+      ),
+      onChanged: (value) {
+        onChanged;
+      },
+      decoration: const InputDecoration(
+        isDense: true,
+        contentPadding: EdgeInsets.all(8),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+            borderSide: BorderSide.none),
+        hintStyle: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+          color: VSColors.ka0a0a0,
+        ),
+        hintText: 'Search country',
+        suffixIconColor: VSColors.ka0a0a0,
+        prefixIcon: Icon(
+          VSIcons.search,
+          size: 20,
+        ),
+        prefixIconColor: VSColors.ka0a0a0,
+        fillColor: VSColors.kf0f0f0,
+        filled: true,
+      ),
+    );
+  }
+}
