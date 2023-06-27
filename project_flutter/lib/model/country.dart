03abef541195f6b7cbd090676575679c3479f109
vansen345 <86304461+vansen345@users.noreply.github.com>
2023-06-27 09:12:19 +0700

    add model

---
+++ project_flutter/lib/model/country.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'country.g.dart';
+
+@JsonSerializable()
+class CountryModel {
+  @JsonKey(name: 'SV501')
+  String? sv501;
+
+  @JsonKey(name: 'SV502')
+  String? sv502;
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  bool isSelected = false;
+
+  CountryModel({
+    this.sv501,
+    this.sv502,
+  });
+
+  factory CountryModel.fromJson(Map<String, dynamic>? json) =>
+      _$CountryModelFromJson(json ?? {});
+
+  Map<String, dynamic> toJson() => _$CountryModelToJson(this);
+}
