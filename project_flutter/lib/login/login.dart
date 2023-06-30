afa510f0db4451fd0d26c49fdf91270020a68f4c
vansen345 <86304461+vansen345@users.noreply.github.com>
2023-06-30 17:52:30 +0700

    errorborder

--- project_flutter/lib/login/login.dart
+++ project_flutter/lib/login/login.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/route_manager.dart';
 import 'package:vansen/gen/colors.gen.dart';
-import 'package:vansen/icon/icons.dart';
 import 'package:vansen/login/login_view_model.dart';
 import 'package:vansen/widgets/app_bar.dart';
+import 'package:vansen/widgets/button.dart';
 import 'package:vansen/widgets/input/input_login.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -35,21 +34,39 @@ class LoginScreen extends GetView<LoginViewModel> {
                 .make()
                 .centered(),
             const HeightBox(30),
-            InputLogin(
-              isShowPass: true,
-              isShow: false,
-              isPassword: true,
-              placeHolder: 'Email/ phone number'.tr,
+            Obx(
+              () => InputLogin(
+                textEditingController: controller.usernameController,
+                isShowPass: true,
+                isShow: false,
+                colorBorder: controller.errorMessage.isNotEmpty
+                    ? Colors.red
+                    : VSColors.ke8e8e8,
+                isPassword: true,
+                placeHolder: 'Email/ phone number'.tr,
+                onChanged: (value) {
+                  controller.validateUserName();
+                },
+                errorMessage: controller.errorMessage.value,
+              ),
             ),
-            const HeightBox(20),
+            const HeightBox(5),
             InputLogin(
               isShowPass: false,
+              errorMessage: '',
+              colorBorder: VSColors.ke8e8e8,
               placeHolder: 'Password'.tr,
               isPassword: false,
               isShow: true,
+              onChanged: (value) {
+                controller.validateUserName();
+              },
+            ).expand(),
+            CustomButton(
+              title: 'Login'.tr,
+              textColor: Colors.white,
             )
           ],
-          crossAlignment: CrossAxisAlignment.center,
         ),
       ),
     );
